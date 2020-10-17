fb1ab8b6e2a35a9bcc8b428a9ac47ddbbf39baae
Neal O'Kelly <neal@recordstransformation.co.uk>
2020-10-17 21:26:02 +0100

    Display classifications as nested <UL>.

--- public/js/components.js
+++ public/js/components.js
@@ -4,13 +4,46 @@ $(document).ready(function(){
 	if($('#classification-treeview').length){
 		$.ajax({url: "get-classifications", success: function(result){
 			var classifications = result;
-			//alert(classifications.Results.length);
-			//alert(classifications.length)
+		var intClassificationsDisplayed = 0;
+		while (intClassificationsDisplayed < classifications.Results.length){
+			
 			for(var i=0; i<classifications.Results.length; i++){
-				if(!classifications.Results[i].hasOwnProperty("ClassificationParentClassification")){
-					$("#classification-treeview ul").append("<li>" + classifications.Results[i].ClassificationName.Value + "</li>");
-						}
+				if(!classifications.Results[i].hasOwnProperty("ClassificationParentClassification"))
+					{
+					if(!$("#classification-uri-" + classifications.Results[i].Uri).length){
+
+					$("#classification-treeview ul").append("<li id='classification-uri-" + classifications.Results[i].Uri + "'><span class='expand-icon'>+</span><span class='folder-icon'></span>" + classifications.Results[i].ClassificationName.Value + "</li>");	
+					intClassificationsDisplayed = intClassificationsDisplayed + 1;
 					}
+					}
+					else{
+						var strNewListItemId = "#classification-uri-" + classifications.Results[i].Uri;
+						console.log(strNewListItemId)
+						
+						var strParentListItemId = "#classification-uri-" + classifications.Results[i].ClassificationParentClassification.Uri;
+						//$(strParentListItemId).addClass("has-children");
+						//$(strParentListItemId).children(".expand-icon").css("background-color","lime")		
+						//$(strParentListItemId).children(".expand-icon").html("+")	
+
+						if(!$(strNewListItemId).length){
+							if($(strParentListItemId).length)
+								{
+								if(!$(strParentListItemId + " ul").length){
+									$(strParentListItemId).append("<ul class='classification-hidden'></ul>");
+								}
+								if(!$("#classification-uri-" + classifications.Results[i].Uri).length)
+									{
+									$(strParentListItemId + " ul").append("<li style='list-style-type:none;' id='classification-uri-" + classifications.Results[i].Uri + "'><span class='expand-icon'>-</span><span class='folder-icon'></span>" + classifications.Results[i].ClassificationName.Value + "</li>")
+									intClassificationsDisplayed++;
+									}
+							}
+						}
+						//console.log($(strParentListItemId + ">ul>li span .expand-icon").html(""));
+						//$(".expand-icon").parent().addClass("has-children");
+
+					}
+			}		
+		}
 				}
 			});
 		}
