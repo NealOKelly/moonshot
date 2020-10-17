90f52eea36d46e8736dfa92bd46e9e5e3bf73a31
Neal O'Kelly <neal@recordstransformation.co.uk>
2020-10-17 20:52:54 +0100

    Display classification as simple, single-level unordered list.

--- public/js/components.js
+++ public/js/components.js
@@ -3,8 +3,13 @@ $(document).ready(function(){
 	// populate the #classification-data div
 	if($('#classification-treeview').length){
 		$.ajax({url: "get-classifications", success: function(result){
-	    $("#classification-treeview").html(JSON.stringify(result));
-  }});
-	}
-	
-})
+			var classifications = result;
+			//alert(classifications.Results.length);
+			//alert(classifications.length)
+			for(var i=0; i<classifications.Results.length; i++){
+				$("#classification-treeview ul").append("<li>" + classifications.Results[i].ClassificationName.Value + "</li>");
+					}
+				}
+			});
+		}
+	})
