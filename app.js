987cb3c27489d1e56489bc8c78d1656ed827c372
Neal O'Kelly <neal@recordstransformation.co.uk>
2020-10-17 15:39:22 +0100

    Classification search added to app.js and working, but not returning in the response html.

--- app.js
+++ app.js
@@ -28,4 +28,51 @@ app.get('', (req, res) => {
 // Listen on Port 300
 app.listen(port, () => console.info('App listening on port ' + port))
 
-//console.log('expressLayouts:' + expressLayouts)//
+///////  CONTENT MANAGER INTEGRATION ///////
+//get dependencies
+const axios = require('axios');
+require('dotenv').config();
+//const dateFormat = require('dateformat');
+
+// constants
+const contentManagerUsername = process.env['CONTENT_MANAGER_USERNAME'];
+const contentManagerPassword = process.env['CONTENT_MANAGER_PASSWORD'];
+const authorizationHeaderValue = "Basic " + Buffer.from(contentManagerUsername + ":" + contentManagerPassword).toString('base64');
+const contentManagerServiceAPIBaseUrl = process.env['CONTENT_MANAGER_API_BASE_URL'];
+
+const agent = require('./modules/https-auth/agent'); 
+
+console.log(contentManagerUsername)
+
+// invoke the CMServiceAPI
+//https://api.gilbyim.com/CMServiceAPI/Classification?q=all&properties=ClassificationName
+function invokeCMServiceAPI()
+	{
+		console.log('invokeCMServiceAPI has been called.')
+		var config = {
+		  httpsAgent: agent('api-client'),
+		  method: 'get',
+		  url: contentManagerServiceAPIBaseUrl + '/Classification?q=all&properties=ClassificationName',
+		  headers: { 
+			'Authorization': authorizationHeaderValue, 
+			//'Content-Type': 'application/json', 
+		  },
+		  //data : JSON.stringify(jsonData)
+		};
+		axios(config)
+		
+		.then(function (response) {
+
+			console.log(response.data)
+			//console.log(getTimeStamp(), green + "New Content Manager record successfully created.", resetColor)
+			
+		})
+		.catch(function (error) {
+		  //console.log(getTimeStamp(), red + "Error creating Content Manager record.", resetColor)
+		  console.log(error);
+		});
+	}
+
+invokeCMServiceAPI()
+
+
