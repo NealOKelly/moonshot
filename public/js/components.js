85180e0b9544d1c7e3a8b45d1aa3347fcdb5481a
Neal O'Kelly <neal@recordstransformation.co.uk>
2020-10-19 16:07:06 +0100

    Classification expand and collapse functionality.

--- public/js/components.js
+++ public/js/components.js
@@ -7,7 +7,6 @@ $(document).ready(function(){
 		var intClassificationsDisplayed = 0;
 		while (intClassificationsDisplayed < classifications.Results.length)
 		{
-			
 			for(var i=0; i<classifications.Results.length; i++){
 				
 				if(!classifications.Results[i].hasOwnProperty("ClassificationParentClassification"))
@@ -21,17 +20,12 @@ $(document).ready(function(){
 					}
 					}
 					else{
-					//var strNewListItemId = "#classification-uri-" + classifications.Results[i].Uri;
-						
-						//console.log("Name: " + classifications.Results[i].ClassificationName.Value + "; ParentName: " + classifications.Results[i].ClassificationParentClassification.ClassificationTitle.Value + "; ParentURI: " + classifications.Results[i].ClassificationParentClassification.Uri + "; Exists: " + $(strParentListItemId).length)
-					
 						var strParentListItemId = "#classification-uri-" + classifications.Results[i].ClassificationParentClassification.Uri;  //get the ID of he parenet Classification.
-						//console.log(strParentListItemId)
 						
-						if($(strParentListItemId).length)  // if lis item for the parent Classification exists
+						if($(strParentListItemId).length)  // if list item for the parent Classification exists
 							{
 							if(!$(strParentListItemId + " ul").length){  // add a new unordered list if none exists.
-								$(strParentListItemId).append("<ul class='classification-hidden' style='list-style-type:none;list-style-position: outside;'></ul>");
+								$(strParentListItemId).append("<ul style='list-style-type:none;list-style-position: outside;'></ul>");
 							}
 							if(!$("#classification-uri-" + classifications.Results[i].Uri).length)
 								{
@@ -42,7 +36,7 @@ $(document).ready(function(){
 					}
 				}		
 		}
-		// sort	
+		// sort	 this list.
 
 		$('ul').each(function(_, ul) {
 		// get all the nodes to be sorted
@@ -51,6 +45,12 @@ $(document).ready(function(){
 		$toSort.each(function() {
 		  $(this).appendTo(ul);
 		});
+	 });
+		// hide everything but top-level classification on load.
+		$("#classification-treeview li").each(function(_, li) {
+			if($(this).parent().parent().is("li")){
+				$(this).addClass("classification-hidden")
+			}
 	 });
 
   	}, error: function(result){
@@ -85,6 +85,36 @@ $(document).on("click", ".classification-name>a", function(){
 	getClassificationProperties(classificationUri)
 })
 
+// Click on collpased caret
+$(document).on("click", ".collapsed", function(){
+	var parentNodeId = $(event.target).parent().attr("id");
+	//alert(parentNodeId)
+	$("#" + parentNodeId +" > ul > li").removeClass("classification-hidden")
+	$("#" + parentNodeId + " > span.collapsed").addClass("expanded")
+	$("#" + parentNodeId +" > span.collapsed").removeClass("collapsed")
+	$("#" + parentNodeId + " > span.folder").addClass("folder-open")
+	$("#" + parentNodeId +" > span.folder").removeClass("folder")
+})
+// Click on expanded caret
+$(document).on("click", ".expanded", function(){
+	var parentNodeId = $(event.target).parent().attr("id");
+	$("#" + parentNodeId + " > span.expanded").addClass("collapsed")
+	$("#" + parentNodeId +" > span.expanded").removeClass("expanded")
+	$("#" + parentNodeId + " > span.folder-open").addClass("folder")
+	$("#" + parentNodeId +" > span.folder-open").removeClass("folder-open")
+})
+
+
+
+
+// Click on expanded caret
+$(document).on("click", ".expanded", function(){
+	var parentNodeId = $(event.target).parent().attr("id");
+	//alert(parentNodeId)
+	$("#" + parentNodeId +" > ul > li").addClass("classification-hidden")
+	//alert($("#" + event.target).attr("id"))
+	//$("#" + event.target + "> ul > li").removeClass("classification-hidden")
+})
 
 // Functions // 
 function highlightSelectedClassification(eventTargetParent){
