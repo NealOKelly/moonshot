2f074150fda0e313df980030ee2bee3324ca5442
Neal O'Kelly <neal@recordstransformation.co.uk>
2020-11-01 14:37:31 +0000

    Moonshot code integrated with SSO code.

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 // Packages
 var passport = require('passport'),
+	expressLayouts = require('express-ejs-layouts'),
 	express = require('express'),
 	https = require('https'),
 	fs = require('fs'),
@@ -7,14 +8,31 @@ var passport = require('passport'),
 	morgan = require('morgan'),
 	cookieParser = require('cookie-parser'),
 	methodOverride = require('method-override'),
-	session = require('express-session');
+	session = require('express-session'),
+	axios = require('axios');
 
 var app = express();
 require('./modules/passport/passport.js');
+const agent = require('./modules/https-auth/agent');
+const contentManagerUsername = process.env['CONTENT_MANAGER_USERNAME'];
+const contentManagerPassword = process.env['CONTENT_MANAGER_PASSWORD'];
+const authorizationHeaderValue = "Basic " + Buffer.from(contentManagerUsername + ":" + contentManagerPassword).toString('base64');
+const contentManagerServiceAPIBaseUrl = process.env['CONTENT_MANAGER_API_BASE_URL'];
+
+
+
+// Static Files
+app.use(express.static('public'))
+app.use('/css', express.static(__dirname + 'public/css'))
+app.use('/js', express.static(__dirname + 'public/js'))
+app.use('/img', express.static(__dirname + 'public/img'))
+
+
 
 // Express configuration
 var router = express.Router();
 app.set('view engine', 'ejs');
+app.use(expressLayouts);
 app.use(morgan('combined'));
 app.use(cookieParser());
 app.use(bodyParser.urlencoded(
@@ -35,7 +53,7 @@ app.use('', router);
 // Routes
 app.get('/', ensureAuthenticated, function(req, res)
 	{
-	res.render('index', { user: req.user, profile: req.profile });
+	res.render('index', { user: req.user, title: "Moonshot" });
 	});
 
 app.get('/account', ensureAuthenticated, function(req, res)
@@ -65,6 +83,96 @@ app.get('/logout', function(req, res)
 	req.logout();
 	res.redirect('/');
 	});
+
+
+// ROUTES - /Search
+app.get("/Search", (req, res, next) =>
+	{
+    console.log("Call to '/Search' received")
+	console.log(req.query.q)
+	var classificationUri = req.query.q
+	var jsonData = {
+					"TrimType": req.query.trimtype,
+					"q": req.query.q,
+					"properties": req.query.properties,
+					"pageSize": 1000000,
+					} 
+	var config = {
+		  httpsAgent: agent('api-client'),
+		  method: 'post',
+		  url: contentManagerServiceAPIBaseUrl + '/Search',
+		  headers:
+			{ 
+			'Authorization': authorizationHeaderValue, 
+			'Content-Type': 'application/json', 
+			},
+		  data: JSON.stringify(jsonData)
+		};
+		//console.log(getTimeStamp(), green + "New Content Manager record successfully created.", resetColor)
+		console.log("Calling CMServiceAPI.")
+		axios(config)
+			.then( function (response)
+			{
+			console.log("Response from CMServiceAPI recieved.")
+			console.log("Sending response to browser.")
+			res.status(200).send(response.data)
+			})
+			.catch(err => next(err));
+	})
+
+
+// get-classification-details
+app.get("/get-classification-details", (req, res, next) => {
+    console.log("Call to '/get-classification-details' received")
+	var classificationUri = req.query.uri
+		
+	var config = {
+		  httpsAgent: agent('api-client'),
+		  method: 'get',
+		  url: contentManagerServiceAPIBaseUrl + '/Classification/' + classificationUri + '/?properties=ClassificationName, ClassificationParentClassification, ClassificationAccessControl, ClassificationRetentionSchedule, ClassificationCanAttachRecords',
+		  headers: { 
+			'Authorization': authorizationHeaderValue, 
+			//'Content-Type': 'application/json', 
+		  },
+		  //data : JSON.stringify(jsonData)
+		};
+  //console.log(getTimeStamp(), green + "New Content Manager record successfully created.", resetColor)
+  console.log("Calling CMServiceAPI.")
+  axios(config)
+    .then( function (response)  {
+	  console.log("Response from CMServiceAPI recieved.")
+	  console.log("Sending response to browser.")
+	  res.status(200).send(response.data)
+  } 
+ )
+    .catch(err => next(err));
+})
+
+// get-record-type-containment-details
+app.get("/get-record-type-attributes", (req, res, next) => {
+    console.log("Call to '/get-record-type-attributes' received")
+	var recordTypeUri = req.query.uri;
+	var config = {
+		  httpsAgent: agent('api-client'),
+		  method: 'get',
+		  url: contentManagerServiceAPIBaseUrl + '/RecordType?q=all&properties=RecordTypeLevel, RecordTypeContentsRule, RecordTypeName',
+		  headers: { 
+			'Authorization': authorizationHeaderValue, 
+		  },
+		};
+  //console.log(getTimeStamp(), green + "New Content Manager record successfully created.", resetColor)
+  console.log("Calling CMServiceAPI.")
+  axios(config)
+    .then( function (response)  {
+	  console.log("Response from CMServiceAPI recieved.")
+	  console.log("Sending response to browser.")
+	  res.status(200).send(response.data)
+  } 
+ )
+    .catch(err => next(err));
+})
+
+
 
 // Create HTTPS listener
 https.createServer(
